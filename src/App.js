4723611de85a923447ad7d1be525d620454efa15
Peter Roto <peterdroto@gmail.com>
2021-08-27 13:21:56 -0500

    trying HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Link, Switch, BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import {Link, Switch, HashRouter as Router, Route, Redirect} from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -12,24 +12,21 @@ function App() {
     <div className="header">
      <Router>
        <nav>
-         <Link to="/react-portfolio/">Home</Link>
-         <Link to="/react-portfolio/projects">Projects</Link>
-         <Link to="/react-portfolio/about">About</Link>
+         <Link to="/">Home</Link>
+         <Link to="/projects">Projects!</Link>
+         <Link to="/about">About</Link>
        </nav>
          <Switch>
-         <Route exact path='/react-portfolio/'>
-             <Redirect to='/react-portfolio/home'></Redirect>
+          <Route exact path="/">
+            <Redirect path="/home"/>
            </Route>
-           <Route exact path='/'>
-             <Redirect to='/react-portfolio/home'></Redirect>
-           </Route>
-             <Route path="/react-portfolio/home">
+             <Route path="/home">
                  <Home />
              </Route>
-             <Route path="/react-portfolio/about">
+             <Route path="/about">
                 <About data={aboutData} />    
              </Route>
-             <Route path="/react-portfolio/projects">
+             <Route path="/projects">
                 <Projects data={projectData}/>
              </Route>
              </Switch>
